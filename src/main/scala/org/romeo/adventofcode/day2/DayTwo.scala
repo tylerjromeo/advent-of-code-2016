64856774e291432633b2cad4482cd54dd0968bce
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-12-08 20:29:02 -0500

    add day two part two solution

--- src/main/scala/org/romeo/adventofcode/day2/DayTwo.scala
+++ src/main/scala/org/romeo/adventofcode/day2/DayTwo.scala
@@ -21,86 +21,207 @@ object DayTwo {
   def main(args: Array[String]): Unit = {
     new DayTwo().run()
   }
+
+  def followDirections(directions: Traversable[String], start: Button): String = {
+    directions.foldLeft(("", start)) {
+      case ((code, startButton), moves) => {
+        val finalPosition = moves.foldLeft(startButton.asInstanceOf[Button])(DayTwo.move)
+        (code + finalPosition.digit, finalPosition)
+      }
+    }._1
+  }
 }
 
 sealed trait Button {
-  val digit: Int
+  val digit: String
   val left: Button
   val right: Button
   val up: Button
   val down: Button
 }
 
-case object One extends Button {
-  override val digit: Int = 1
-  override val left: Button = One
-  override val right: Button = Two
-  override val up: Button = One
-  override val down: Button = Four
+object KeypadOne {
+
+  case object One extends Button {
+    override val digit: String = "1"
+    override val left: Button = One
+    override val right: Button = Two
+    override val up: Button = One
+    override val down: Button = Four
+  }
+
+  case object Two extends Button {
+    override val digit: String = "2"
+    override val left: Button = One
+    override val right: Button = Three
+    override val up: Button = Two
+    override val down: Button = Five
+  }
+
+  case object Three extends Button {
+    override val digit: String = "3"
+    override val left: Button = Two
+    override val right: Button = Three
+    override val up: Button = Three
+    override val down: Button = Six
+  }
+
+  case object Four extends Button {
+    override val digit: String = "4"
+    override val left: Button = Four
+    override val right: Button = Five
+    override val up: Button = One
+    override val down: Button = Seven
+  }
+
+  case object Five extends Button {
+    override val digit: String = "5"
+    override val left: Button = Four
+    override val right: Button = Six
+    override val up: Button = Two
+    override val down: Button = Eight
+  }
+
+  case object Six extends Button {
+    override val digit: String = "6"
+    override val left: Button = Five
+    override val right: Button = Six
+    override val up: Button = Three
+    override val down: Button = Nine
+  }
+
+  case object Seven extends Button {
+    override val digit: String = "7"
+    override val left: Button = Seven
+    override val right: Button = Eight
+    override val up: Button = Four
+    override val down: Button = Seven
+  }
+
+  case object Eight extends Button {
+    override val digit: String = "8"
+    override val left: Button = Seven
+    override val right: Button = Nine
+    override val up: Button = Five
+    override val down: Button = Eight
+  }
+
+  case object Nine extends Button {
+    override val digit: String = "9"
+    override val left: Button = Eight
+    override val right: Button = Nine
+    override val up: Button = Six
+    override val down: Button = Nine
+  }
+
 }
 
-case object Two extends Button {
-  override val digit: Int = 2
-  override val left: Button = One
-  override val right: Button = Three
-  override val up: Button = Two
-  override val down: Button = Five
-}
+object KeypadTwo {
 
-case object Three extends Button {
-  override val digit: Int = 3
-  override val left: Button = Two
-  override val right: Button = Three
-  override val up: Button = Three
-  override val down: Button = Six
-}
+  case object One extends Button {
+    override val digit: String = "1"
+    override val left: Button = One
+    override val right: Button = One
+    override val up: Button = One
+    override val down: Button = Three
+  }
 
-case object Four extends Button {
-  override val digit: Int = 4
-  override val left: Button = Four
-  override val right: Button = Five
-  override val up: Button = One
-  override val down: Button = Seven
-}
+  case object Two extends Button {
+    override val digit: String = "2"
+    override val left: Button = Two
+    override val right: Button = Three
+    override val up: Button = Two
+    override val down: Button = Six
+  }
 
-case object Five extends Button {
-  override val digit: Int = 5
-  override val left: Button = Four
-  override val right: Button = Six
-  override val up: Button = Two
-  override val down: Button = Eight
-}
+  case object Three extends Button {
+    override val digit: String = "3"
+    override val left: Button = Two
+    override val right: Button = Four
+    override val up: Button = One
+    override val down: Button = Seven
+  }
 
-case object Six extends Button {
-  override val digit: Int = 6
-  override val left: Button = Five
-  override val right: Button = Six
-  override val up: Button = Three
-  override val down: Button = Nine
-}
+  case object Four extends Button {
+    override val digit: String = "4"
+    override val left: Button = Three
+    override val right: Button = Four
+    override val up: Button = Four
+    override val down: Button = Eight
+  }
 
-case object Seven extends Button {
-  override val digit: Int = 7
-  override val left: Button = Seven
-  override val right: Button = Eight
-  override val up: Button = Four
-  override val down: Button = Seven
-}
+  case object Five extends Button {
+    override val digit: String = "5"
+    override val left: Button = Five
+    override val right: Button = Six
+    override val up: Button = Five
+    override val down: Button = Five
+  }
 
-case object Eight extends Button {
-  override val digit: Int = 8
-  override val left: Button = Seven
-  override val right: Button = Nine
-  override val up: Button = Five
-  override val down: Button = Eight
-}
+  case object Six extends Button {
+    override val digit: String = "6"
+    override val left: Button = Five
+    override val right: Button = Seven
+    override val up: Button = Two
+    override val down: Button = A
+  }
 
-case object Nine extends Button {
-  override val digit: Int = 9
-  override val left: Button = Eight
-  override val right: Button = Nine
-  override val up: Button = Six
-  override val down: Button = Nine
+  case object Seven extends Button {
+    override val digit: String = "7"
+    override val left: Button = Six
+    override val right: Button = Eight
+    override val up: Button = Three
+    override val down: Button = B
+  }
+
+  case object Eight extends Button {
+    override val digit: String = "8"
+    override val left: Button = Seven
+    override val right: Button = Nine
+    override val up: Button = Four
+    override val down: Button = C
+  }
+
+  case object Nine extends Button {
+    override val digit: String = "9"
+    override val left: Button = Eight
+    override val right: Button = Nine
+    override val up: Button = Nine
+    override val down: Button = Nine
+  }
+
+  case object A extends Button {
+    override val digit: String = "A"
+    override val left: Button = A
+    override val right: Button = B
+    override val up: Button = Six
+    override val down: Button = A
+  }
+
+  case object B extends Button {
+    override val digit: String = "B"
+    override val left: Button = A
+    override val right: Button = C
+    override val up: Button = Seven
+    override val down: Button = D
+  }
+
+  case object C extends Button {
+    override val digit: String = "C"
+    override val left: Button = B
+    override val right: Button = C
+    override val up: Button = Eight
+    override val down: Button = C
+  }
+
+  case object D extends Button {
+    override val digit: String = "D"
+    override val left: Button = D
+    override val right: Button = D
+    override val up: Button = B
+    override val down: Button = D
+  }
+
 }
 
 class DayTwo extends Puzzle("http://adventofcode.com/2016/day/2/input") {
@@ -119,15 +240,10 @@ class DayTwo extends Puzzle("http://adventofcode.com/2016/day/2/input") {
     * @param input
     * @return
     */
-override def solvePart1(input: String): String = {
-  val directions = DayTwo.splitInput(input)
-  directions.foldLeft(("", Five.asInstanceOf[Button])){
-    case ((code, startButton), moves) => {
-      val finalPosition = moves.foldLeft(startButton.asInstanceOf[Button])(DayTwo.move)
-      (code + finalPosition.digit, finalPosition)
-    }
-  }._1
-}
+  override def solvePart1(input: String): String = {
+    val directions = DayTwo.splitInput(input)
+    DayTwo.followDirections(directions, KeypadOne.Five.asInstanceOf[Button])
+  }
 
   /**
     * solve part 2 of the day's problem
@@ -135,5 +251,8 @@ override def solvePart1(input: String): String = {
     * @param input
     * @return
     */
-  override def solvePart2(input: String): String = ???
+  override def solvePart2(input: String): String = {
+    val directions = DayTwo.splitInput(input)
+    DayTwo.followDirections(directions, KeypadTwo.Five.asInstanceOf[Button])
+  }
 }
