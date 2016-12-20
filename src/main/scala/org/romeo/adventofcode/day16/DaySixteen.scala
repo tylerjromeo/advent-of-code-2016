99f24a4039b0acd89719a97a61a26a7818b6a5d9
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-12-19 21:10:47 -0500

    add day sixteen part two

--- src/main/scala/org/romeo/adventofcode/day16/DaySixteen.scala
+++ src/main/scala/org/romeo/adventofcode/day16/DaySixteen.scala
@@ -16,25 +16,24 @@ object DaySixteen {
   }
 
   def dragonCurve(a: String): String = {
-    a + '0' + a.reverse.map{
-      case '0'=> '1'
-      case '1'=> '0'
+    a + '0' + a.reverse.map {
+      case '0' => '1'
+      case '1' => '0'
     }
   }
 
   @tailrec
   def checksum(s: String): String = {
     val retval = s.grouped(2).map(s => {
-      if(s(0) == s(1)) {
+      if (s(0) == s(1)) {
         '1'
       } else {
         '0'
       }
     }).mkString
-    if(retval.length % 2 == 1) retval else checksum(retval)
+    if (retval.length % 2 == 1) retval else checksum(retval)
   }
 }
-
 
 
 class DaySixteen extends Puzzle("http://adventofcode.com/2016/day/16/input") {
@@ -53,14 +52,14 @@ class DaySixteen extends Puzzle("http://adventofcode.com/2016/day/16/input") {
     * @param input
     * @return
     */
-override def solvePart1(input: String): String = {
-  val diskSize = 272
-  var pad = input
-  while(pad.length < diskSize) {
-    pad = DaySixteen.dragonCurve(pad)
+  override def solvePart1(input: String): String = {
+    val diskSize = 272
+    var pad = input
+    while (pad.length < diskSize) {
+      pad = DaySixteen.dragonCurve(pad)
+    }
+    DaySixteen.checksum(pad.take(diskSize))
   }
-  DaySixteen.checksum(pad.take(diskSize))
-}
 
   /**
     * solve part 2 of the day's problem
@@ -68,5 +67,12 @@ override def solvePart1(input: String): String = {
     * @param input
     * @return
     */
-  override def solvePart2(input: String): String = ???
+  override def solvePart2(input: String): String = {
+    val diskSize = 35651584
+    var pad = input
+    while (pad.length < diskSize) {
+      pad = DaySixteen.dragonCurve(pad)
+    }
+    DaySixteen.checksum(pad.take(diskSize))
+  }
 }
